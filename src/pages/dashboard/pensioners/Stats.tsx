dcbb35e38598558148165c1a7189f8ea2f3e3e0c
olamide.olaitan <olamide.olaitan@etranzact.com>
2023-09-11 15:23:42 +0100

    dashboard layout fix 2

--- src/pages/dashboard/pensioners/Stats.tsx
+++ src/pages/dashboard/pensioners/Stats.tsx
@@ -1,6 +1,6 @@
 function Stats() {
     return (
-        <div className="grid w-full grid-cols-3 gap-2 space-y-1 rounded border-s-8 border-amber-400 bg-neutral-50 p-1 px-0 text-xs text-gray-400 sm:grid-cols-4">
+        <div className="grid w-full grid-cols-3 gap-2 space-y-1 rounded border-s-8 border-green-400 bg-neutral-50 p-1 px-0 text-xs text-gray-400 sm:grid-cols-4">
             <div className="space-y-1 px-2 pt-1">
                 <div className=" font-medium text-slate-700">Name</div>
                 <div>Joseph Aminu</div>
